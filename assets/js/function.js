6cc88639e5aa7f8ed25c801806b89b5b3b778bf1
Md.Ashraful Alam <101570492+ashraful-commits@users.noreply.github.com>
2022-07-24 18:24:42 +0600

    function edit

--- assets/js/function.js
+++ assets/js/function.js
@@ -23,3 +23,53 @@ const getItem = (key) => {
     return false;
   }
 };
+
+const setAlert = (msg, type = "danger") => {
+  return `<h5 class="alert alret-${type}">${msg}</h5>`;
+};
+
+const setDataLs = (key, value) => {
+  let data = [];
+  if (localStorage.getItem(key)) {
+    data = JSON.parse(localStorage.getItem(key));
+  }
+
+  data.push(value);
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+const updataLsData = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getItem = (key) => {
+  if (localStorage.getItem(key)) {
+    return JSON.parse(localStorage.getItem(key));
+  } else {
+    return false;
+  }
+};
+
+// timeCounter function
+
+const timeCounter = (postTime) => {
+  let currentTime = Date.now();
+  let timeDiff = currentTime - postTime;
+
+  let totalSecond = Math.floor(timeDiff / 1000);
+  let totalMin = Math.floor(totalSecond / 60);
+  let totalHour = Math.floor(totalMin / 60);
+  let totalDay = Math.floor(totalHour / 24);
+
+  if (totalSecond <= 60) {
+    return `${totalSecond} s`;
+  } else if (totalSecond > 60) {
+    return `${totalMin} m`;
+  } else if (totalMin > 60) {
+    return `${totalHour} h`;
+  } else if (totalHour > 24) {
+    return `${totalDay} d`;
+  } else {
+    return `old post`;
+  }
+};
